c7af450abfa641b2c76914d216878c6de8bffffc
habibur.shabu@gmail.com <habibur.shabu@gmail.com>
2018-09-29 03:15:00 +0600

    Models Complete

---
+++ LibraryManagementProj/Models/BookIssue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagementProj.Models
+{
+    public class BookIssue
+    {
+        [Required]
+        public int MemberId { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        [Required]
+        public DateTime DateIssue { get; set; }
+
+        [Required]
+        public DateTime DateReturn { get; set; }
+
+    }
+}
---
+++ LibraryManagementProj/Models/Payment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagementProj.Models
+{
+    public class Payment
+    {
+        [Required]
+        public int MemberId { get; set; }
+
+        [Required]
+        public float Amount { get; set; }
+
+        [Required]
+        public DateTime PaymentDate { get; set; }
+
+        [Required]
+        public string PaymentType { get; set; }
+
+
+    }
+}
